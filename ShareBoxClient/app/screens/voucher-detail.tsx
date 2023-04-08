951da13c5aab62ae03b496ed21f9c5bd74471608
Nguyen M Ho <nguyen.hominh@employmenthero.com>
2023-04-08 19:47:49 +0700

    feat: handle upload voucher image

--- ShareBoxClient/app/screens/voucher-detail.tsx
+++ ShareBoxClient/app/screens/voucher-detail.tsx
@@ -1,8 +1,18 @@
 import {useMutation, useQuery} from '@tanstack/react-query';
 import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {tokenSelector, userSelector} from '../state/auth-slice';
 import {useAppSelector} from '../state/hook';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {Field, Form} from 'react-final-form';
+import Input from '../components/text-input';
 
 const VoucherDetail: (props: any) => JSX.Element = ({route}) => {
   const {voucherId} = route.params;
@@ -11,6 +21,7 @@ const VoucherDetail: (props: any) => JSX.Element = ({route}) => {
   const [voucher, setVoucher] = useState<any>({id: voucherId});
 
   const {isLoading: isFetchingVoucher} = useQuery({
+    queryKey: ['voucher_request'],
     queryFn: () => {
       return fetch(
         `http://localhost:3000/api/v1/voucher_requests/${voucherId}`,
@@ -46,6 +57,29 @@ const VoucherDetail: (props: any) => JSX.Element = ({route}) => {
     },
   });
 
+  const uploadImage = useMutation({
+    mutationFn: ({imageUri}: any) => {
+      const formData = new FormData();
+      formData.append('image', {
+        uri: imageUri,
+        name: 'image.jpg', // Replace with the actual image file name
+        type: 'image/jpeg', // Replace with the actual image file type
+      });
+
+      return fetch(
+        `http://localhost:3000/api/v1/voucher_requests/${voucherId}`,
+        {
+          method: 'PATCH',
+          headers: {
+            Authorization: `bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+          body: formData,
+        },
+      );
+    },
+  });
+
   const isOwnRequest = voucher.user_id === user?.id;
   const isHandleRequest = voucher.taken_by_user_id === user?.id;
   const isRequestHandled = !!voucher.taken_by_user_id;
@@ -61,6 +95,11 @@ const VoucherDetail: (props: any) => JSX.Element = ({route}) => {
       </View>
     );
   }
+
+  const onSubmit = (values: any) => {
+    const {photo} = values;
+    uploadImage.mutate({imageUri: photo});
+  };
 
   if (isHandleRequest) {
     return (
@@ -83,6 +122,61 @@ const VoucherDetail: (props: any) => JSX.Element = ({route}) => {
         <Text style={{textAlign: 'center', fontSize: 18, color: '#227C70'}}>
           You are handled this request. Please fill the voucher detail below
         </Text>
+
+        <Form onSubmit={onSubmit}>
+          {({handleSubmit}) => (
+            <View>
+              <Field name="photo">
+                {({input}) => {
+                  return !!input.value ? (
+                    <>
+                      <Image
+                        source={{uri: input.value}}
+                        style={{width: 200, height: 200, alignSelf: 'center'}}
+                      />
+                    </>
+                  ) : (
+                    <Button
+                      title={'Upload Image'}
+                      onPress={async () => {
+                        const result = await launchImageLibrary({
+                          mediaType: 'photo',
+                        });
+                        const assets = result?.assets || [];
+                        const imageUri = assets[0].uri;
+                        input.onChange(imageUri);
+                      }}
+                    />
+                  );
+                }}
+              </Field>
+
+              <View style={{alignSelf: 'center'}}>
+                <TouchableOpacity
+                  onPress={handleSubmit}
+                  style={{
+                    // this to wrap the width of the content
+                    alignSelf: 'baseline',
+                    backgroundColor: '#227C70',
+                    borderRadius: 8,
+                    marginTop: 16,
+                    paddingHorizontal: 24,
+                    paddingVertical: 16,
+                  }}>
+                  <Text
+                    style={{
+                      textAlign: 'center',
+                      fontSize: 20,
+                      color: '#fff',
+                      fontWeight: '600',
+                    }}>
+                    Submit
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </Form>
       </View>
     );
   }
